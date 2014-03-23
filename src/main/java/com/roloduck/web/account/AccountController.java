66e080361bbcf0da59743e9de3ac055aaec7012d
Peter <pemjohns@gmail.com>
2014-03-23 00:57:34 -0500

    Initial routing for profile and settings pages

---
+++ src/main/java/com/roloduck/web/account/AccountController.java
@@ -0,0 +1,28 @@
+package com.roloduck.web.account;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author Andrew Ertell
+ * @author Peter Johnston
+ * @since 3/22/14
+ * RoloDuck
+ */
+
+@Controller
+public class AccountController {
+
+
+    @RequestMapping(value = "/profile", method = RequestMethod.GET)
+    public String serveProfilePage(ModelMap model) {
+        return "profile";
+    }
+
+    @RequestMapping(value = "/settings", method = RequestMethod.GET)
+    public String serveSettingsPage(ModelMap model) {
+        return "settings";
+    }
+}
