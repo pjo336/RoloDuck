4b88b0f20fba3b1298fc0ad1adeba5ca5e4bd612
Peter <pemjohns@gmail.com>
2014-05-03 16:53:20 -0500

    Added routing for specific partner contact creation

--- src/main/java/com/roloduck/web/controller/contact/ContactController.java
+++ src/main/java/com/roloduck/web/controller/contact/ContactController.java
@@ -14,10 +14,7 @@ import com.roloduck.web.exception.ProcessException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponseWrapper;
@@ -71,9 +68,32 @@ public class ContactController extends ProcessException {
         return "contacts";
     }
 
+    @RequestMapping(value = URI_PREFIX + "/create/partner={partnerStr}", method = RequestMethod.GET)
+    public String serveContactCreatePopulatedPartner(@PathVariable String partnerStr, ModelMap model) {
+        User user = null;
+        Company company = null;
+        try {
+            user = SecurityUtils.getCurrentUser();
+        } catch(ServiceLogicException sle) {
+            processRDException(model, sle);
+            return "redirect:/";
+        }
+        try {
+            company = companyService.restoreCompanyByUser(user);
+            model.addAttribute("companyName", company.getCompanyName());
+            List<Partner> companyPartners = partnerService.findAllCompanyPartners(company.getId());
+            model.addAttribute("partners", companyPartners);
+            model.addAttribute("partnerSelected", Long.valueOf(partnerStr));
+            return "contacts-create";
+        } catch(ServiceLogicException sle) {
+            processRDException(model, sle);
+            return "redirect:/";
+        }
+    }
 
     @RequestMapping(value = URI_PREFIX + "/create", method = RequestMethod.GET)
     public String serveContactCreate(ModelMap model) {
+        System.out.println("AHHHH");
         User user = null;
         Company company = null;
         try {
